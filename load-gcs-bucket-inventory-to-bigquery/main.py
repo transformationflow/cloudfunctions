3ed6710edcc0e2f070225cc1bd638393a47369e9
jimbeepbeep <jim@beepbeep.technology>
2022-02-16 15:07:54 +0100

    Updated with file exists check

--- load-gcs-bucket-inventory-to-bigquery/main.py
+++ load-gcs-bucket-inventory-to-bigquery/main.py
@@ -19,16 +19,15 @@ def main_function(event, context=None):
 
         # get objects in bucket
         bucket_objects = GCS.list_blobs(gcs_bucket_name)
-        #bucket_objects_list = list(bucket_objects)
 
-        # truncate BQ table
-        truncate_table_query = f"TRUNCATE TABLE `{destination_inventory_table_ref}´"
-        result = BQ.query(query=truncate_table_query)
+        # get file names and hashes currently in inventory
+        get_current_inventory_query = f"SELECT name, md5Hash FROM `{destination_inventory_table_ref}`"
+        result = BQ.query(query=get_current_inventory_query)
+        current_inventory_names = [row['name'] for row in result]
+        current_inventory_hashes = {row['name']: row['md5Hash'] for row in result}
+
         table = BQ.get_table(destination_inventory_table_ref)
 
-        # stream object properties into empty BQ table
-        error_count = 0
-        errors_list = []
         for bucket_object in bucket_objects:
             object_properties = bucket_object._properties
             
@@ -37,24 +36,99 @@ def main_function(event, context=None):
                 object_properties["metadata"] = json.dumps([{"key": k, "value": v} for k, v in object_properties["metadata"].items()])
             else:
                 object_properties["metadata"] = None
-            
-            rows_to_insert = [object_properties]
 
-            errors = BQ.insert_rows(
-                table, rows_to_insert, row_ids=[None] * len(rows_to_insert)
-            )
-            
-            if len(errors) > 0:
-                print(f"error loading file information: {object_properties['bucket']}/{object_properties['name']}")
-                error_count += 1
-                errors_list.append(object_properties['name'])
+            # if file name does not exist then stream row to bq
+            if object_properties['name'] not in current_inventory_names:
+                #print(object_properties['name'])
+                errors = BQ.insert_rows(table, [object_properties], row_ids=[None] * len(object_properties))
+                #print(f"streamed with {len(errors)} errors {object_properties['name']}. errors: {errors}:")
 
-            else:    
-                print(f"success loading file information: {object_properties['bucket']}/{object_properties['name']}") 
-            break
+            # if file name exists and hash is different then stream row to bq
+            elif object_properties['name'] in current_inventory_names and current_inventory_hashes[object_properties['name']] != object_properties['md5Hash']:
+                #print(object_properties['name'], ": ", object_properties['md5Hash'])
+                errors = BQ.insert_rows(table, [object_properties], row_ids=[None] * len(object_properties))
+                #print(f"streamed updated info with {len(errors)} errors: {object_properties['name']}. errors: {errors}")
+
+            #else:
+                #print(f"file with matching hash {current_inventory_hashes[object_properties['name']]} already exists: {object_properties['name']} ")
+
+
+
+            
+            
+            
+
+                
+
+
+        # if file name exists and hash is the same, do nothing
+
+
+
+        # truncate BQ table
+        # truncate_table_query = f"TRUNCATE TABLE `{destination_inventory_table_ref}´"
+        #truncate_table_query = f"EXECUTE IMMEDIATE (SELECT `flowus.create.empty_gcs_bucket_inventory_table_ddl`('{destination_inventory_table_ref}'));"
+        #print(truncate_table_query)
+        #result = BQ.query(query=truncate_table_query)
+        #print(result)
+        #print(type(result))
+        #print(dir(result))
+        #print(result.state)
+
+        #table = BQ.get_table(destination_inventory_table_ref)
+
+        # stream object properties into empty BQ table
+        #error_count = 0
+        #errors_list = []
+        #all_rows = []
+       # for bucket_object in bucket_objects:
+            #object_properties = bucket_object._properties
+            
+            # json encode optional object metadata
+            #if "metadata" in object_properties:
+            #    object_properties["metadata"] = json.dumps([{"key": k, "value": v} for k, v in object_properties["metadata"].items()])
+            #else:
+            #    object_properties["metadata"] = None
+            
+            #rows_to_insert = [object_properties]
+            #all_rows.append(object_properties)
+            #errors = BQ.insert_rows(
+            #    table, rows_to_insert, row_ids=[None] * len(rows_to_insert)
+            #)
+        #print(f"{len(all_rows)} rows to be loaded in 5000 row sets")
+
+        #def divide_chunks(l, n):
+        #     for i in range(0, len(l), n): 
+        #        yield l[i:i + n]
+
+        # divide list into chunks of 5000
+        # all_rows_chunked = divide_chunks(all_rows, 5000)
+
+        # load chunks to bigquery
+        # for loading_rows in all_rows_chunked:
+        #    errors = BQ.insert_rows(table, loading_rows, row_ids=[None] * len(loading_rows))
+        #    print(f"errors: {errors}")
+
+
+        #print(all_rows)
+
+
+        #    if len(errors) > 0:
+        #        print(f"error loading file information: {object_properties['bucket']}/{object_properties['name']}")
+        #        error_count += 1
+        #        errors_list.append(object_properties['name'])
+
+        #    else:    
+        #        print(f"success loading file information: {object_properties['bucket']}/{object_properties['name']}") 
+            
+            
+            
 
     except Exception as e:
         print("Exception:", e)   
+
+
+
 
 
 # LOCAL TESTING
